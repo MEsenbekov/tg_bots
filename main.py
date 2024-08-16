65af79b47b9e554e8b72b8e77de306ff1f70dc0e
MEsenbekov <maksatdaniar30@gmail.com>
2024-08-16 21:17:13 +0600

    dz 3-4-5

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 # from aiogram import types
 import logging
-from config import dp, bot
+import config
 from aiogram.utils import executor
 from handlers import commands, echo, game, FSM_reg, FSM_store, send_products
 from db import db_main
 
-admin = [668334970]
+admin = [1196009647]
 
 
 async def on_startup(_):
     for i in admin:
-        await bot.send_message(chat_id=i, text='bot on')
+        await config.bot.send_message(chat_id=i, text='Бот включен')
         await db_main.sql_create()
 
 
 async def on_shutdown(_):
     for i in admin:
-        await bot.send_message(chat_id=i, text='bot off')
+        await config.bot.send_message(chat_id=i, text='Бот выкдючен')
 
 
-commands.register_commands(dp)
-game.register_quiz(dp)
-FSM_reg.register_fsm_store(dp)
-FSM_store.store_fsm(dp)
-send_products.register_send_products_handler(dp)
-echo.register_echo(dp)
+commands.register_commands(config.dp)
+game.register_quiz(config.dp)
+FSM_reg.register_fsm_store(config.dp)
+FSM_store.store_fsm(config.dp)
+send_products.register_send_products_handler(config.dp)
+echo.register_echo(config.dp)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(config.dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
